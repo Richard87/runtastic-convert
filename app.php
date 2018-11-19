256c2773532762b60b8b477d186aa39fd467e7b2
Richard Hagen <richard.hagen@gmail.com>
2018-11-19 10:56:53 +0100

    init commit

---
+++ app.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * ./Sport-sessions/{UUID.json}                 - Description of workout
+ * ./Sport-sessions/Elevation-data/{UUID.json}  - Description of elevation
+ * ./Sport-sessions/GPS-data/{UUID.json}        - Description of GPS
+ * ./Sport-sessions/Heart-rate-data{UUID.json}  - Description of workout
+ *
+*/
+
+CONST SPORT_TYPE_RUNNING = 1;
